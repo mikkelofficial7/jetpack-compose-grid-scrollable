aba764c8bc6d7efc35b6e59e51bd45bc4673db1b
Mikkel <mikkelseptiano86@gmail.com>
2024-04-15 22:22:48 +0700

    add orientation filter

--- app/src/main/java/com/example/jetpackscrollable/network/ApiClient.kt
+++ app/src/main/java/com/example/jetpackscrollable/network/ApiClient.kt
@@ -9,6 +9,7 @@ interface ApiClient {
     suspend fun getAllPhotos(
         @Query("client_id") clientId: String = RetrofitClient.getClientId(),
         @Query("page") page: Int = 1,
-        @Query("per_page") itemPage: Int = 10
+        @Query("per_page") itemPage: Int = 10,
+        @Query("orientation") orientation: String = "portrait"
     ): PhotoResponse
 }
